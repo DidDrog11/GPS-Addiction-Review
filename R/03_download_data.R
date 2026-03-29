c77b9cfccce289c1ee04523d8481d3be6109cf5b
OlgaPerski <38756739+OlgaPerski@users.noreply.github.com>
2026-03-29 17:16:58 +0200

    adding cleaning and analysis scripts

---
+++ R/03_download_data.R
@@ -0,0 +1,16 @@
+# read in data from google drive
+
+gs4_auth()
+
+# main extraction sheets
+
+df1 <- read_sheet("https://docs.google.com/spreadsheets/d/1Qczi3KtAgCIcSWKmEm6tOqCBO0hUHxmlhUXBiCTkcxQ/edit?gid=2016267919#gid=2016267919")
+df2 <- read_sheet("https://docs.google.com/spreadsheets/d/1NXWH9WJAKAZ6TtDWxxju2Jk_1xBb017P70X4Xzg9mWk/edit?gid=2016267919#gid=2016267919")
+
+df_combined <- rbind(df1, df2)
+
+# quality appraisal - observational
+
+# quality appraisal - prepost
+
+# quality appraisal - experimental
